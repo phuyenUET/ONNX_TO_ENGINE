8bdc098a1c4410f5b919c1389c0c9d20e23a55ba
m15take <asuka1475369@gmail.com>
2025-10-29 10:07:46 +0700

    chỉnh lại dynamic = False

--- pt_to_onnx.py
+++ pt_to_onnx.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model_path = r"E:\OneDrive - vnu.edu.vn\Desktop\GitHub\Do_An\runs\classify\train3\weights\best.pt"
+model_path = r"E:\OneDrive - vnu.edu.vn\Desktop\GitHub\ONNX_TO_ENGINE\runs\classify\train3\weights\best.pt"
 
 model = YOLO(model_path)
 
@@ -8,6 +8,6 @@ model.export(
     format="onnx",
     opset=12,                # Phiên bản ONNX, khuyến nghị 12 hoặc 13
     simplify=True,           # Tối ưu ONNX model (xóa các node dư thừa)
-    dynamic=True,            # Hỗ trợ kích thước ảnh linh hoạt
+    dynamic=False,            # Hỗ trợ kích thước ảnh linh hoạt
     imgsz=640                # Kích thước input ảnh
 )
